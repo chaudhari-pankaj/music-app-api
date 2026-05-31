60022ea4da390e9e06f7d2011a2548da7ff1b3b4
chaudhari-pankaj <chaudharigpankaj@gmail.com>
2026-05-31 16:04:58 +0530

    refactor : updated ytdl-core-enhanced package and fixed breaking changes

--- services/songService.js
+++ services/songService.js
@@ -1,10 +1,20 @@
 const ytdl = require('ytdl-core-enhanced');
 
+//package now mostly only returns one item so filtering is not possible
+//so we let the package decide the best item
+// export const streamSong = async (videoId,response) => {
+//     ytdl(videoId,{
+//         quality: 'highestaudio',
+//         filter : 'audioonly',
+//     }).pipe(response);
+// }
 const streamSong = async (videoId,response) => {
-    ytdl(videoId,{
-        quality: 'highestaudio',
-        filter : 'audioonly',
-    }).pipe(response);
+    try{
+        ytdl(videoId).pipe(response);
+    }
+    catch(error) {
+        console.log(error);
+    }
 }
 
 const getSongInfo = async (videoId) => {
